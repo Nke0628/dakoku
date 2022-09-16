4953a21af460dc0e4863dc5dac8e44a637351f1b
Nke0628 <ooo_diva@yahoo.co.jp>
2022-09-16 20:16:37 +0900

    ログイン画面の文言修正

--- frontend/src/components/pages/LoginPage.vue
+++ frontend/src/components/pages/LoginPage.vue
@@ -7,24 +7,19 @@
         </b-row>
         <b-row class="mt-4">
             <b-col cols="12" class="text-left">
-                アカウントとパスワードでログインしてください。<br>
+                メールアドレスとパスワードでログインしてください。<br />
             </b-col>
         </b-row>
         <b-row class="mt-3 font-weight-bold">
-            <b-col cols="12" class="title-text">email</b-col>
+            <b-col cols="12" class="title-text">メールアドレス</b-col>
             <b-col cols="12">
-                <b-input
-                    v-model="loginForm.email"
-                />
+                <b-input v-model="loginForm.email" />
             </b-col>
         </b-row>
         <b-row class="mt-3 font-weight-bold">
-            <b-col cols="12" class="title-text">password</b-col>
+            <b-col cols="12" class="title-text">パスワード</b-col>
             <b-col cols="12">
-                <b-input
-                    v-model="loginForm.password"
-                    type="password"
-                />
+                <b-input v-model="loginForm.password" type="password" />
             </b-col>
         </b-row>
         <b-row>
@@ -35,8 +30,8 @@
                     block
                     class="mt-5"
                     variant="primary"
-                >ログイン
-                    <b-spinner small v-if="loadingLogin"/>
+                    >ログイン
+                    <b-spinner small v-if="loadingLogin" />
                 </b-button>
             </b-col>
         </b-row>
@@ -44,49 +39,49 @@
 </template>
 
 <script>
-import {mapActions} from "vuex";
+import { mapActions } from "vuex";
 export default {
     name: "LoginPage",
     data() {
         return {
             loginForm: {
-                email: '',
-                password: ''
+                email: "",
+                password: "",
             },
-            loadingLogin: false
-        }
+            loadingLogin: false,
+        };
     },
     methods: {
         ...mapActions({
-            actionRequestLogin: 'AuthorizedUsers/actionRequestLogin'
+            actionRequestLogin: "AuthorizedUsers/actionRequestLogin",
         }),
         async handleLogin() {
-            if ( this.loginForm.email === '' ) {
-                alert('emailが入力されておりません。');
+            if (this.loginForm.email === "") {
+                alert("emailが入力されておりません。");
                 return;
             }
-            if ( this.loginForm.password === '' ) {
-                alert('パスワードが入力されておりません。');
+            if (this.loginForm.password === "") {
+                alert("パスワードが入力されておりません。");
                 return;
             }
             this.loadingLogin = true;
             await this.actionRequestLogin(this.loginForm);
             this.loadingLogin = false;
         },
-    }
-}
+    },
+};
 </script>
 
 <style scoped>
-    .LoginPage {
-        margin-top: 2rem;
-    }
+.LoginPage {
+    margin-top: 2rem;
+}
 
-    h1 .main-title {
-        text-align: center;
-    }
+h1 .main-title {
+    text-align: center;
+}
 
-    .title-text {
-        text-align: left;
-    }
+.title-text {
+    text-align: left;
+}
 </style>
