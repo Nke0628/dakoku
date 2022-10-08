5503d823d93d4cd140e3f93b121a773d6220d51e
Nke0628 <ooo_diva@yahoo.co.jp>
2022-10-08 09:00:17 +0900

    認証情報をリロードして維持するように修正

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import AuthorizedUsers from './AuthorizedUsers';
+import Vue from "vue";
+import Vuex from "vuex";
+import * as Cookies from "js-cookie";
+import createPersistedState from "vuex-persistedstate";
+import AuthorizedUsers from "./AuthorizedUsers";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-    modules : {
-        AuthorizedUsers
-    }
-})
+    modules: {
+        AuthorizedUsers,
+    },
+    plugins: [
+        createPersistedState({
+            key: "timecard",
+            paths: ["AuthorizedUsers"],
+            storage: {
+                getItem: (key) => Cookies.get(key),
+                setItem: (key, value) =>
+                    Cookies.set(key, value, {
+                        expires: 7,
+                        secure: false,
+                    }),
+                removeItem: (key) => Cookies.remove(key),
+            },
+        }),
+    ],
+});
